4b0a84d65a05dcb4524011904411c3530beced4f
paul <paul@conversionready.com>
2019-05-02 21:53:34 +0300

    wip cxl-catalog

--- packages/cxl-ui/src/index.js
+++ packages/cxl-ui/src/index.js
@@ -1,4 +1,8 @@
+/* Cards */
 export { CXLBannerCard} from './components/cxl-banner-card.js';
 export { CXLCourseCard } from './components/cxl-course-card.js';
 export { CXLLessonCard } from './components/cxl-lesson-card.js';
 export { CXLTestimonialCard } from './components/cxl-testimonial-card.js';
+
+/* Layouts */
+export { CXLCatalog } from './components/cxl-catalog.js';
