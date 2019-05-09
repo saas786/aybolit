3ad007c4c1b246e265dbd4e0e716af6a66666a5f
paul <paul@conversionready.com>
2019-05-09 17:59:11 +0300

    lesson card template update

--- packages/cxl-ui/src/components/cxl-lesson-card.js
+++ packages/cxl-ui/src/components/cxl-lesson-card.js
@@ -6,6 +6,35 @@ export class CXLLessonCard extends CXLBaseCard {
   static get styles() {
     return [super.styles, lessonCardStyles];
   }
+
+  static get properties() {
+    return {
+      thumbnail: {
+        type: String
+      }
+    };
+  }
+
+  render() {
+    return html`
+      <header>
+        <p class="lesson-thumbnail" name="lesson-thumbnail" style="background-image: url('${this.thumbnail}');"></p>
+        <h2>
+          <slot name="entry-title-highlighted"></slot>
+          <slot name="entry-title"></slot>
+        </h2>
+        <p class="lesson-specs">
+          <slot name="lesson-specs"></slot>
+        </p>
+      </header>
+      <div class="content">
+        <p>
+          <slot></slot>
+        </p>        
+      </div>
+    `;
+  }
+
 }
 
 customElements.define('cxl-lesson-card', CXLLessonCard);
