6542aa2135d84ff8b54e68b9d534c0b6a3a7d577
Janar <janar.palk@gmail.com>
2019-05-14 20:54:47 +0300

    feat(cxl-institute-sidebar-item): add course menu
    fix: header name wording

--- packages/storybook/layouts.stories.js
+++ packages/storybook/layouts.stories.js
@@ -28,7 +28,7 @@ storiesOf('Layouts', module)
         </cxl-icon-nav>
 
         <!-- Sidebar -->
-        <h3 slot="sidebar-header">Training Manager</h3>
+        <h3 slot="sidebar-header" part="sidebar-header">Training Manager</h3>
         <div class="sidebar-content" slot="sidebar-content">
           <strong class="extra-heading">Digital analytics minidegree program</strong>
           <vaadin-details theme="cxl-sidebar-track" opened>
@@ -101,6 +101,147 @@ storiesOf('Layouts', module)
             Course
             <vaadin-button theme="primary" slot="header-button">
               Optional button
+            </vaadin-button>
+          </cxl-institute-content-header>
+
+          <div class="embed-container" slot="content-video">
+            <iframe
+              src="https://player.vimeo.com/video/321840987"
+              height="400"
+              frameborder="0"
+              webkitallowfullscreen
+              mozallowfullscreen
+              allowfullscreen
+            ></iframe>
+          </div>
+
+          <h2>Betrayed cheerful declared end and</h2>
+
+          <p>
+            Certainty listening no no behaviour existence assurance situation is. Because add why
+            not esteems amiable him. Interested the unaffected mrs law friendship add principles.
+            Indeed on people do merits to. Court heard which up above hoped grave do. Answer living
+            law things either sir bed length. Looked before we an on merely. These no death he at
+            share alone. Yet outward the him compass hearted are tedious.
+          </p>
+
+          <p>
+            Written enquire painful ye to offices forming it. Then so does over sent dull on.
+            Likewise offended humoured mrs fat trifling answered. On ye position greatest so
+            desirous. So wound stood guest weeks no terms up ought. By so these am so rapid blush
+            songs begin. Nor but mean time one over.
+          </p>
+
+          <p>
+            Savings her pleased are several started females met. Short her not among being any.
+            Thing of judge fruit charm views do. Miles mr an forty along as he. She education get
+            middleton day agreement performed preserved unwilling. Do however as pleased offence
+            outward beloved by present. By outward neither he so covered amiable greater. Juvenile
+            proposal betrayed he an informed weddings followed. Precaution day see imprudence
+            sympathize principles. At full leaf give quit to in they up.
+          </p>
+
+          <p>
+            Situation admitting promotion at or to perceived be. Mr acuteness we as estimable
+            enjoyment up. An held late as felt know. Learn do allow solid to grave. Middleton
+            suspicion age her attention. Chiefly several bed its wishing. Is so moments on chamber
+            pressed to. Doubtful yet way properly answered humanity its desirous. Minuter believe
+            service arrived civilly add all. Acuteness allowance an at eagerness favourite in
+            extensive exquisite ye.
+          </p>
+
+          <p>
+            In to am attended desirous raptures declared diverted confined at. Collected instantly
+            remaining up certainly to necessary as. Over walk dull into son boy door went new. At or
+            happiness commanded daughters as. Is handsome an declared at received in extended
+            vicinity subjects. Into miss on he over been late pain an. Only week bore boy what fat
+            case left use. Match round scale now sex style far times. Your me past an much.
+          </p>
+
+          <p>
+            Arrived compass prepare an on as. Reasonable particular on my it in sympathize. Size now
+            easy eat hand how. Unwilling he departure elsewhere dejection at. Heart large seems may
+            purse means few blind. Exquisite newspaper attending on certainty oh suspicion of. He
+            less do quit evil is. Add matter family active mutual put wishes happen.
+          </p>
+        </cxl-institute-content>
+      </cxl-institute-layout>
+    `;
+  })
+  .add('cxl-institute-layout (course progress)', () => {
+    return html`
+      <cxl-institute-layout>
+        <cxl-icon-nav slot="icon-nav">
+          <cxl-icon-nav-item href="#">
+            <iron-icon class="icon size-l" icon="cxl:nav-trainings"></iron-icon>
+          </cxl-icon-nav-item>
+          <cxl-icon-nav-item href="#">
+            <iron-icon class="icon size-l" icon="cxl:nav-roadmap"></iron-icon>
+          </cxl-icon-nav-item>
+          <cxl-icon-nav-item href="#">
+            <iron-icon class="icon size-l" icon="cxl:nav-discover"></iron-icon>
+          </cxl-icon-nav-item>
+          <cxl-icon-nav-item
+            href="https://conversionxli.warmpress.com/my-account/"
+            position="bottom"
+          >
+            <iron-icon class="icon size-l" icon="lumo:user"></iron-icon>
+          </cxl-icon-nav-item>
+        </cxl-icon-nav>
+
+        <!-- Sidebar -->
+        <h3 slot="sidebar-header" part="sidebar-header">Active Training</h3>
+        <div class="sidebar-content" slot="sidebar-content">
+          <vaadin-details theme="cxl-sidebar-track" opened>
+            <div slot="summary">8 Common Testing Mistakes and How to Avoid Them</div>
+            <div>
+              <cxl-institute-sidebar-items>
+                <cxl-institute-sidebar-item>
+                  <a href="#" class="active">Importance of correcting mistakes</a>
+                </cxl-institute-sidebar-item>
+
+                <cxl-institute-sidebar-item>
+                  <a href="#">Mistake #1: not letting a test run long enough</a>
+                </cxl-institute-sidebar-item>
+
+                <cxl-institute-sidebar-item>
+                  <a href="#">Mistake #2: testing too many small elements</a>
+                </cxl-institute-sidebar-item>
+
+                <cxl-institute-sidebar-item>
+                  <a href="#">Mistake #3: just testing random things</a>
+                </cxl-institute-sidebar-item>
+
+                <cxl-institute-sidebar-item>
+                  <a href="#">Mistake #4: false positives</a>
+                </cxl-institute-sidebar-item>
+
+                <cxl-institute-sidebar-item>
+                  <a href="#">Mistake #5: not knowing when to say when</a>
+                </cxl-institute-sidebar-item>
+
+                <cxl-institute-sidebar-item>
+                  <a href="#">Mistake #6: failing to optimize for each traffic source</a>
+                </cxl-institute-sidebar-item>
+
+                <cxl-institute-sidebar-item>
+                  <a href="#">Mistake #7: only focusing on conversion rate</a>
+                </cxl-institute-sidebar-item>
+
+                <cxl-institute-sidebar-item>
+                  <a href="#">Mistake #8: treating low traffic websites the same</a>
+                </cxl-institute-sidebar-item>
+              </cxl-institute-sidebar-items>
+            </div>
+          </vaadin-details>
+        </div>
+
+        <!-- Content -->
+        <cxl-institute-content>
+          <cxl-institute-content-header with-button slot="content-header">
+            Importance of correcting mistakes
+            <vaadin-button theme="primary" slot="header-button">
+              Start course
             </vaadin-button>
           </cxl-institute-content-header>
 
