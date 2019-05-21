ea4479bdc6dbfdb6e7819b407a24b60399438e32
leho <leho@conversionready.com>
2019-05-21 14:36:05 +0300

    refactor(cxl-ui): move theme modules closer to their owner, sanitize everything

--- packages/cxl-ui/src/components/cxl-minidegree-progress-widget/cxl-vaadin-accordion.js
+++ packages/cxl-ui/src/components/cxl-minidegree-progress-widget/cxl-vaadin-accordion.js
@@ -5,7 +5,6 @@ class CXLVaadinAccordion extends AccordionElement {
   _updateOpened(e) {
     const target = this._filterItems(e.composedPath())[0];
     const idx = this.items.indexOf(target);
-
     if (e.detail.value) {
       if (target.disabled || idx === -1) {
         return;
@@ -15,53 +14,9 @@ class CXLVaadinAccordion extends AccordionElement {
     } else if (!this.items.some(item => item.opened)) {
       this.opened = null;
     }
-
-    this._saveAccordionState(this.items, e.detail.value, idx);
   }
 
-  _updateItems(items) {
-    if (!items) {
-      return;
-    }
-
-    if (!this.hasAppliedState) {
-      const accordionId = this.id;
-      this._applyAccordionState(items, accordionId);
-    }
-
-    this.hasAppliedState = true;
-  }
-
-  _saveAccordionState(items) {
-    const accordionId = this.id;
-    const stateItems = this._getPanelsFromState();
-    const newPanelState = [];
-
-    items.forEach(function(value, key) {
-      newPanelState[key] = items[key].opened;
-    });
-
-    stateItems[accordionId] = newPanelState;
-
-    localStorage.setItem('cxl-vaadin-accordion-panels', JSON.stringify(stateItems));
-  }
-
-  _applyAccordionState(items, accordionId) {
-    const stateItems = this._getPanelsFromState();
-
-    if (!stateItems[accordionId]) {
-      return;
-    }
-
-    items.forEach(function(item, key) {
-      const currentItem = item;
-      currentItem.opened = stateItems[accordionId][key];
-    });
-  }
-
-  _getPanelsFromState() {
-    return JSON.parse(localStorage.getItem('cxl-vaadin-accordion-panels')) || {};
-  }
+  _updateItems() {}
 }
 
 customElements.define('cxl-vaadin-accordion', CXLVaadinAccordion);
--- packages/storybook/templates/cxl-minidegree-progress-widget.js
+++ packages/storybook/templates/cxl-minidegree-progress-widget.js
@@ -1,157 +1,80 @@
 import { html } from 'lit-html';
 
 export default html`
-  <cxl-minidegree-progress-widget id="post-1001" class="entry entry--type-certificate">
-
-    <cxl-vaadin-accordion id="vaadin-accordion-1001">
-
-      <vaadin-accordion-panel id="post-1011" class="entry entry--type-step" theme="cxl-track reverse">
-
-        <vaadin-item theme="cxl-track" slot="summary">
-          <label>Track #1</label>
-          <h3 class="entry__title">Google Analytics</h3>
-        </vaadin-item>
-
-        <cxl-course-progress-widget id="post-2022" class="entry entry--type-course">
-
-          <cxl-vaadin-accordion id="cxl-vaadin-accordion-2022">
-
-            <vaadin-accordion-panel theme="cxl-course">
-
-              <vaadin-item slot="summary">
-                <h4 class="entry__title">Analytics fundamentals (w/ modules)</h4>
-              </vaadin-item>
-
-              <vaadin-tabs theme="cxl-course minimal" orientation="vertical" selected="-1">
-
-                <vaadin-tab>
-                  <h6 class="entry__title"><a href="">Course introduction</a></h6>
-                </vaadin-tab>
-                <vaadin-tab disabled>
-                  <h5 class="module__title">Main module</h5>
-                </vaadin-tab>
-                <vaadin-tab>
-                  <h6 class="entry__title"><a href="">Analytics fundamentals</a></h6>
-                </vaadin-tab>
-                <vaadin-tab>
-                  <h6 class="entry__title"><a href="">Getting started with Google Analytics</a></h6>
-                </vaadin-tab>
-                <vaadin-tab>
-                  <h6 class="entry__title"><a href="">Who are my visitors?</a></h6>
-                </vaadin-tab>
-                <vaadin-tab>
-                  <h6 class="entry__title"><a href="">Where are my visitors coming from?</a></h6>
-                </vaadin-tab>
-                <vaadin-tab disabled>
-                  <h5 class="module__title">Final exam</h5>
-                </vaadin-tab>
-                <vaadin-tab>
-                  <h6 class="entry__title"><a href="">Final exam</a></h6>
-                </vaadin-tab>
-
-              </vaadin-tabs>
-
-            </vaadin-accordion-panel>
-
-          </cxl-vaadin-accordion>
-
-        </cxl-course-progress-widget>
-
-        <cxl-course-progress-widget>
-
-          <cxl-vaadin-accordion id="cxl-vaadin-accordion-2023">
-
-            <vaadin-accordion-panel id="post-2023" class="entry entry--type-course" theme="cxl-course">
-
-              <vaadin-item slot="summary">
-                <h4 class="entry__title">Google Analytics for beginners</h4>
-              </vaadin-item>
-
-              <vaadin-tabs theme="cxl-course minimal" orientation="vertical" selected="-1">
-
-                <vaadin-tab>
-                  <h6 class="entry__title"><a href="">Course introduction</a></h6>
-                </vaadin-tab>
-                <vaadin-tab disabled>
-                  <h5 class="module__title">Main module</h5>
-                </vaadin-tab>
-                <vaadin-tab>
-                  <h6 class="entry__title"><a href="">Getting started with Google Analytics</a></h6>
-                </vaadin-tab>
-                <vaadin-tab>
-                  <h6 class="entry__title"><a href="">Who are my visitors?</a></h6>
-                </vaadin-tab>
-                <vaadin-tab>
-                  <h6 class="entry__title"><a href="">Where are my visitors coming from?</a></h6>
-                </vaadin-tab>
-                <vaadin-tab>
-                  <h6 class="entry__title"><a href="">What are the results of my visitors' actions?</a></h6>
-                </vaadin-tab>
-                <vaadin-tab disabled>
-                  <h5 class="module__title">Final exam</h5>
-                </vaadin-tab>
-                <vaadin-tab>
-                  <h6 class="entry__title"><a href="">Final Exam - Google Analytics for Beginners</a></h6>
-                </vaadin-tab>
-              </vaadin-tabs>
-
-            </vaadin-accordion-panel>
-
-          <cxl-vaadin-accordion>
-
-        </cxl-course-progress-widget>
-
+  <cxl-minidegree-progress-widget>
+    <vaadin-accordion-panel theme="cxl-track">
+      <vaadin-item theme="cxl-track-summary" slot="summary">
+        <label>Track #1</label>
+        <h4 class="entry__title">Google Analytics</h4>
+      </vaadin-item>
+      <vaadin-accordion-panel theme="cxl-course">
+        <div slot="summary">Analytics fundamentals</div>
+        <vaadin-list-box theme="cxl-course-links">
+          <h4 class="module-header">Analytics fundamentals</h4>
+          <a href="">Analytics fundamentals</a>
+        </vaadin-list-box>
       </vaadin-accordion-panel>
-
-      <vaadin-accordion-panel id="post-1012" class="entry entry--type-step" theme="cxl-track reverse">
-
-        <vaadin-item theme="cxl-track" slot="summary">
-          <label>Track #2</label>
-          <h3 class="entry__title">Presenting data</h3>
-        </vaadin-item>
-
-        <cxl-course-progress-widget>
-
-          <cxl-vaadin-accordion id="cxl-vaadin-accordion-2024">
-
-            <vaadin-accordion-panel id="post-2024" class="entry entry--type-course" theme="cxl-course">
-
-              <vaadin-item slot="summary">
-                <h4 class="entry__title">Data presentation &amp; visualization (no modules)</h4>
-              </vaadin-item>
-
-              <vaadin-tabs theme="cxl-course minimal" orientation="vertical" selected="-1">
-
-                <vaadin-tab>
-                  <h6 class="entry__title"><a href="">Course introduction</a></h6>
-                </vaadin-tab>
-                <vaadin-tab>
-                  <h6 class="entry__title"><a href="">Introduction + Conceptualize</a></h6>
-                </vaadin-tab>
-                <vaadin-tab>
-                  <h6 class="entry__title"><a href="">Visualize: design like Andy Warhol</a></h6>
-                </vaadin-tab>
-                <vaadin-tab>
-                  <h6 class="entry__title"><a href="">Visualize: data design</a></h6>
-                </vaadin-tab>
-                <vaadin-tab>
-                  <h6 class="entry__title"><a href="">Analytics strategy: the process for successful digital analytics</a></h6>
-                </vaadin-tab>
-                <vaadin-tab>
-                  <h6 class="entry__title"><a href="">Analytics strategy: socializing analytical results for enabling business decisions</a></h6>
-                </vaadin-tab>
-
-              </vaadin-tabs>
-
-            </vaadin-accordion-panel>
-
-          </cxl-vaadin-accordion>
-
-        <cxl-course-progress-widget>
-
+      <vaadin-accordion-panel theme="cxl-course">
+        <div slot="summary">Google Analytics for beginners</div>
+        <vaadin-list-box theme="cxl-course-links">
+          <h4 class="module-header">Google Analytics for beginners</h4>
+          <a href="">Getting started with Google Analytics</a>
+          <a href="">Who are my visitors?</a>
+          <a href="">Where are my visitors coming from?</a>
+          <a href="">What are the results of my visitors' actions?</a>
+          <h4 class="module-header">Final exam</h4>
+          <a href="">Final Exam - Google Analytics for Beginners</a>
+        </vaadin-list-box>
       </vaadin-accordion-panel>
-
-    </cxl-vaadin-accordion>
-
+    </vaadin-accordion-panel>
+    <vaadin-accordion-panel theme="cxl-track">
+      <vaadin-item theme="cxl-track-summary" slot="summary">
+        <label>Track #2</label>
+        <h4 class="entry__title">Presenting data</h4>
+      </vaadin-item>  
+      <vaadin-accordion-panel theme="cxl-course">
+        <div slot="summary">Data presentation &amp; visualization</div>
+        <vaadin-list-box theme="cxl-course-links">
+          <h4 class="module-header">Data presentation &amp; visualization</h4>
+          <a href="">Introduction + Conceptualize</a>
+          <a href="">Visualize: design like Andy Warhol</a>
+          <a href="">Visualize: data design</a>
+          <a href="">Analytics strategy: the process for successful digital analytics</h4>
+          <a href="">Analytics strategy: socializing analytical results for enabling business decisions</a>
+        </vaadin-list-box>
+      </vaadin-accordion-panel>
+    </vaadin-accordion-panel>
+    <vaadin-accordion-panel theme="cxl-track">
+      <vaadin-item theme="cxl-track-summary" slot="summary">
+        <label>Track #3</label>
+        <h4 class="entry__title">Google Tag Manager</h4>
+      </vaadin-item>
+      <vaadin-accordion-panel theme="cxl-course">
+        <div slot="summary">Data presentation &amp; visualization</div>
+        <vaadin-list-box theme="cxl-course-links">
+          <a href="">Introduction + Conceptualize</a>
+          <a href="">Visualize: design like Andy Warhol</a>
+          <a href="">Visualize: data design</a>
+          <a href="">Analytics strategy: the process for successful digital analytics</h4>
+          <a href="">Analytics strategy: socializing analytical results for enabling business decisions</a>
+        </vaadin-list-box>
+      </vaadin-accordion-panel>
+    </vaadin-accordion-panel>
+    <vaadin-accordion-panel theme="cxl-track">
+      <vaadin-item theme="cxl-track-summary" slot="summary">
+        <label>Track #4</label>
+        <h4 class="entry__title">Attribution</h4>
+      </vaadin-item>  
+      <vaadin-accordion-panel theme="cxl-course">
+        <div slot="summary">Data presentation &amp; visualization</div>
+        <vaadin-list-box theme="cxl-course-links">
+          <a href="">Introduction + Conceptualize</a>
+          <a href="">Visualize: design like Andy Warhol</a>
+          <a href="">Visualize: data design</a>
+          <a href="">Analytics strategy: the process for successful digital analytics</h4>
+          <a href="">Analytics strategy: socializing analytical results for enabling business decisions</a>
+        </vaadin-list-box>
+      </vaadin-accordion-panel>
+    </vaadin-accordion-panel>
   </cxl-minidegree-progress-widget>
 `;
