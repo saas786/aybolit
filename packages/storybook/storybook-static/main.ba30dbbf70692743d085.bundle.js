7c654834a93058ea53ce9fe980f71e7d038f9578
Janar <janar.palk@gmail.com>
2019-06-19 19:57:31 +0300

    chore: add storybook-static globally to make deploy faster
    chore: remove gitignore for storybook-static (faster deployment)
    
    chore: add new ignore files

---
+++ packages/storybook/storybook-static/main.ba30dbbf70692743d085.bundle.js
@@ -0,0 +1,13 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([
+  [0],
+  {
+    0(n, o, p) {
+      p(396), p(480), (n.exports = p(854));
+    },
+    480(n, o, p) {
+      p.r(o);
+      p(481), p(850);
+    }
+  },
+  [[0, 1, 2]]
+]);
