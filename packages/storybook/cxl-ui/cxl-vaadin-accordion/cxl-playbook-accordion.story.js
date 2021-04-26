49687e51484cd25c687089496a636725115a9b22
Dominik Stypuła <dominik.stypula@polcode.net>
2021-04-26 17:17:53 +0200

    feat(storybook): cxl-playbook-accordion.story add feedback button
    This is a combination of 9 commits.
    
    feat(cxl-playbook-accordion): hover icon for feedback
    feat(cxl-playbook-accordion): css adjustments
    fix(cxl-playbook-accordion): comment button click stop propagation
    fix(cxl-lumo-styles/icons.js): @font-face and --vaadin-icons-comment updated for vaadin:comment icon
    feat(cxl-playbook-accordion.story): leave feedback button
    feat(cxl-playbook-accordion.story): javascripts for hover temporarily commented, css updated
    refactor(cxl-playbook-accordion.story): no anonymous containers
    refactor(cxl-playbook-accordion.story): hovering comment icon dropped
    refactor(cxl-playbook-accordion.story): redundant class dropped

--- packages/storybook/cxl-ui/cxl-vaadin-accordion/cxl-playbook-accordion.story.js
+++ packages/storybook/cxl-ui/cxl-vaadin-accordion/cxl-playbook-accordion.story.js
@@ -6,15 +6,24 @@ export const CXLPlaybookAccordion = () => html`
   <cxl-playbook-accordion id="playbook-3895" class="plural" opened="0">
     ${playbookStepData.map(
       (el) => html`
-        <vaadin-accordion-panel class="playbook-step" theme="cxl-playbook-accordion reverse">
+        <vaadin-accordion-panel
+          class="playbook-step"
+          theme="cxl-playbook-accordion reverse"
+          data-step-id="${el.id}"
+        >
           <header class="entry-header" slot="summary">
             <vaadin-checkbox value="${el.id}"></vaadin-checkbox>
             <h3 class="entry-title no-anchor" itemprop="headline">
               ${el.id}. ${unsafeHTML(el.title.rendered)}
             </h3>
           </header>
-
           <div class="entry-summary" itemprop="description">${unsafeHTML(el.content.rendered)}</div>
+          <div class="entry-footer">
+            <vaadin-button>
+              <iron-icon icon="vaadin:comment" slot="prefix"></iron-icon>
+              Leave Feedback
+            </vaadin-button>
+          </div>
         </vaadin-accordion-panel>
       `
     )}
