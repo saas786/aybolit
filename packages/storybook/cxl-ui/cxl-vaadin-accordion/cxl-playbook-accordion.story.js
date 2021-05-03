c03b63b6ee0ec99dec3a455ab41752a06e7ba0d2
Leho Kraav <leho@kraav.com>
2021-05-03 18:09:08 +0300

    refactor(storybook): cxl-playbook-accordion.story add FeedbackButtonLabel arg

--- packages/storybook/cxl-ui/cxl-vaadin-accordion/cxl-playbook-accordion.story.js
+++ packages/storybook/cxl-ui/cxl-vaadin-accordion/cxl-playbook-accordion.story.js
@@ -2,15 +2,11 @@ import { html } from 'lit-html';
 import { unsafeHTML } from 'lit-html/directives/unsafe-html';
 import playbookStepData from './cxl-playbook-accordion.data.json';
 
-export const CXLPlaybookAccordion = () => html`
+export const CXLPlaybookAccordion = ({ FeedbackButtonLabel }) => html`
   <cxl-playbook-accordion id="playbook-3895" class="plural" opened="0">
     ${playbookStepData.map(
       (el) => html`
-        <vaadin-accordion-panel
-          class="playbook-step"
-          theme="cxl-playbook-accordion reverse"
-          data-step-id="${el.id}"
-        >
+        <vaadin-accordion-panel theme="cxl-playbook-accordion reverse" data-step-id="${el.id}">
           <header class="entry-header" slot="summary">
             <vaadin-checkbox value="${el.id}"></vaadin-checkbox>
             <h3 class="entry-title no-anchor" itemprop="headline">
@@ -19,9 +15,11 @@ export const CXLPlaybookAccordion = () => html`
           </header>
           <div class="entry-summary" itemprop="description">${unsafeHTML(el.content.rendered)}</div>
           <div class="entry-footer">
-            <vaadin-button>
+            <vaadin-button
+              onclick="alert('Step ID: ' + this.closest('vaadin-accordion-panel').dataset.stepId)"
+            >
               <iron-icon icon="vaadin:comment" slot="prefix"></iron-icon>
-              Leave Feedback
+              ${FeedbackButtonLabel}
             </vaadin-button>
           </div>
         </vaadin-accordion-panel>
